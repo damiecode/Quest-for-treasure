72d643acedf07801fc5139ca12a732e84f606032
damiecode <codenlyn@gmail.com>
2020-03-10 10:30:09 +0100

    Add webpack configuration and generate files

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+/* eslint-disable-next-line import/no-unresolved */
+
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+    'production-dependencies': ['phaser'],
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].js',
+  },
+  optimization: {
+    minimize: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src/'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, 'assets', '**', '*'),
+        to: path.resolve(__dirname, 'build'),
+      },
+    ]),
+    new webpack.DefinePlugin({
+      'typeof CANVAS_RENDERER': JSON.stringify(true),
+      'typeof WEBGL_RENDERER': JSON.stringify(true),
+    }),
+  ],
+
+  devServer: {
+    contentBase: path.resolve(__dirname, 'build'),
+  },
+};
