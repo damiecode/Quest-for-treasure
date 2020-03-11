df1ff2d14850c85edf71c9e818ee5fb294aedfc8
damiecode <codenlyn@gmail.com>
2020-03-11 19:13:08 +0100

    Adjust progress bar x-axis

--- src/scenes/preloaderScene.js
+++ src/scenes/preloaderScene.js
@@ -15,7 +15,7 @@ export default class PreloaderScene extends Phaser.Scene {
     const progressBar = this.add.graphics();
     const progressBox = this.add.graphics();
     progressBox.fillStyle(0x222222, 0.8);
-    progressBox.fillRect(350, 270, 320, 50);
+    progressBox.fillRect(240, 270, 320, 50);
 
     const { width } = this.cameras.main;
     const { height } = this.cameras.main;
