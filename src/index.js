825502a328666ddeb1ba9188b06c4d065152e63a
damiecode <codenlyn@gmail.com>
2020-03-11 19:29:44 +0100

    Add title scene to index file

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import Phaser from 'phaser';
 
 import BootScene from './scenes/bootScene';
 import PreloaderScene from './scenes/preloaderScene';
+import TitleScene from './scenes/titleScene';
 import GameScene from './scenes/gameScene';
 
 import config from './config';
 
 const gameConfig = Object.assign(config, {
-  scene: [BootScene, PreloaderScene, GameScene],
+  scene: [BootScene, PreloaderScene, TitleScene, GameScene],
 });
 
 class Game extends Phaser.Game {
