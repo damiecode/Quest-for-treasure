0891e943e1eadeacb142829bc2bb6e3ec38c35a3
damiecode <codenlyn@gmail.com>
2020-03-11 20:06:29 +0100

    Update the options scene

--- src/scenes/preloaderScene.js
+++ src/scenes/preloaderScene.js
@@ -84,10 +84,10 @@ export default class PreloaderScene extends Phaser.Scene {
   }
 
   ready() {
-    this.scene.start('TitleScene');
+    this.scene.start('OptionsScene');
     this.readyCount += 1;
     if (this.readyCount === 2) {
-      this.scene.start('OptionsScene');
+      this.scene.start('TitleScene');
     }
   }
 }
