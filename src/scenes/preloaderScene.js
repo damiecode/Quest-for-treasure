e7d47f9fc7a1de5b7a19f2f6e27923bfb47db169
damiecode <codenlyn@gmail.com>
2020-03-11 19:34:22 +0100

    Add title scene to index file

--- src/scenes/preloaderScene.js
+++ src/scenes/preloaderScene.js
@@ -83,10 +83,10 @@ export default class PreloaderScene extends Phaser.Scene {
   }
 
   ready() {
-    this.scene.start('OptionsScene');
+    this.scene.start('TitleScene');
     this.readyCount += 1;
     if (this.readyCount === 2) {
-      this.scene.start('TitleScene');
+      this.scene.start('OptionsScene');
     }
   }
 }
