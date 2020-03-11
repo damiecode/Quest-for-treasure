5378d8c9ec73fc89a01ef1507ccc3db35e0b4daf
damiecode <codenlyn@gmail.com>
2020-03-11 19:06:34 +0100

    Change logo position

--- src/scenes/preloaderScene.js
+++ src/scenes/preloaderScene.js
@@ -10,7 +10,7 @@ export default class PreloaderScene extends Phaser.Scene {
   }
 
   preload() {
-    this.add.image(500, 200, 'logo');
+    this.add.image(460, 200, 'logo');
 
     const progressBar = this.add.graphics();
     const progressBox = this.add.graphics();
