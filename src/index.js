74c4145d4fb5fe5a8823aba6170f26d3f53523e5
damiecode <codenlyn@gmail.com>
2020-03-11 18:55:19 +0100

    Add preloader for game

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@
 import Phaser from 'phaser';
 
 import BootScene from './scenes/bootScene';
-import SplashScene from './scenes/splashScene';
+import PreloaderScene from './scenes/preloaderScene';
 import GameScene from './scenes/gameScene';
 
 import config from './config';
 
 const gameConfig = Object.assign(config, {
-  scene: [BootScene, SplashScene, GameScene],
+  scene: [BootScene, PreloaderScene, GameScene],
 });
 
 class Game extends Phaser.Game {
