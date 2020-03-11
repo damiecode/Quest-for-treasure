023abf9ee1e27a47fb4bfd70577b76b04bff8246
damiecode <codenlyn@gmail.com>
2020-03-11 19:26:33 +0100

    Add play button and options button

---
+++ src/scenes/titleScene.js
@@ -0,0 +1,60 @@
+import 'phaser';
+import config from '../config';
+
+export default class TitleScene extends Phaser.Scene {
+  constructor() {
+    super('TitleScene');
+  }
+
+  create() {
+    this.gameButton = this.add.sprite(100, 200, 'blueButton1').setInteractive();
+    this.centerButton(this.gameButton, 1);
+
+    this.gameText = this.add.text(0, 0, 'Play', {
+      fontSize: '32px',
+      fill: '#fff',
+    });
+    this.centerButtonText(this.gameText, this.gameButton);
+
+    this.gameButton.on(
+      'pointerdown',
+      (pointer) => {
+        this.scene.start('Game');
+      },
+    );
+
+    this.optionsButton = this.add
+      .sprite(300, 200, 'blueButton1')
+      .setInteractive();
+    this.centerButton(this.optionsButton);
+
+    this.optionsText = this.add.text(0, 0, 'Options', {
+      fontSize: '32px',
+      fill: '#fff',
+    });
+    this.centerButtonText(this.optionsText, this.optionsButton);
+
+    this.optionsButton.on(
+      'pointerdown',
+      (pointer) => {
+        this.scene.start('Options');
+      },
+    );
+  }
+
+  centerButton(gameObject, offset = 0) {
+    Phaser.Display.Align.In.Center(
+      gameObject,
+      this.add.zone(
+        config.width / 2,
+        config.height / 2 - offset * 100,
+        config.width,
+        config.height,
+      ),
+    );
+  }
+
+  centerButtonText(gameText, gameButton) {
+    Phaser.Display.Align.In.Center(gameText, gameButton);
+  }
+}
