3ded5e63660080fb7047968b23f8b4463335164c
damiecode <codenlyn@gmail.com>
2020-03-12 21:19:02 +0100

    Add main character to the game

---
+++ src/animations/heroanimations.js
@@ -0,0 +1,21 @@
+export default function makeAnimations() {
+  this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  this.anims.create({
+    key: 'turn',
+    frames: [{ key: 'dude', frame: 4 }],
+    frameRate: 20,
+  });
+
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+}
--- src/scenes/gameScene.js
+++ src/scenes/gameScene.js
@@ -1,12 +1,5 @@
 import Phaser from 'phaser';
-
-function Hero(game, x, y) {
-  Phaser.Sprite.call(this, game, x, y, 'hero');
-  this.anchor.set(0.5, 0.5);
-}
-
-Hero.prototype = Object.create(Phaser.Sprite.prototype);
-Hero.prototype.constructor = Hero;
+import makeAnimations from '../animations/heroanimations';
 
 export default class extends Phaser.Scene {
   constructor() {
@@ -22,7 +15,12 @@ export default class extends Phaser.Scene {
     this.load.image('grass:4x1', './assets/images/grass_4x1.png');
     this.load.image('grass:2x1', './assets/images/grass_2x1.png');
     this.load.image('grass:1x1', './assets/images/grass_1x1.png');
-    this.load.image('hero', './assets/images/hero_stopped.png');
+    this.load.spritesheet('dude',
+      './assets/images/dude.png',
+      { frameWidth: 32, frameHeight: 48 });
+    this.load.on('complete', () => {
+      makeAnimations(this);
+    });
   }
 
   create() {
@@ -32,15 +30,9 @@ export default class extends Phaser.Scene {
 
   loadLevel(data) {
     data.platforms.forEach(this.spawnPlatform, this);
-    this.spawnCharacters({ hero: data.hero });
   }
 
   spawnPlatform(platform) {
     this.add.sprite(platform.x, platform.y, platform.image);
-  }
-
-  spawnCharacters(data) {
-    this.hero = new Hero(this.game, data.hero.x, data.hero.y);
-    this.game.add.existing(this.hero);
   }
 }
