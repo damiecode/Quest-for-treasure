caf103b7267b31c7235d4484c765573f9e4e28c1
damiecode <codenlyn@gmail.com>
2020-03-12 09:53:22 +0100

    Add main character to game scene

--- src/scenes/gameScene.js
+++ src/scenes/gameScene.js
@@ -6,7 +6,7 @@ export default class extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('background', './assets/images/sky.png');
+    this.load.image('background', './assets/images/background.png');
     this.load.json('level:1', './assets/data/level01.json');
     this.load.image('ground', './assets/images/ground.png');
     this.load.image('grass:8x1', './assets/images/grass_8x1.png');
@@ -14,11 +14,17 @@ export default class extends Phaser.Scene {
     this.load.image('grass:4x1', './assets/images/grass_4x1.png');
     this.load.image('grass:2x1', './assets/images/grass_2x1.png');
     this.load.image('grass:1x1', './assets/images/grass_1x1.png');
+    this.load.spritesheet('dude',
+      './assets/images/dude.png',
+      { frameWidth: 32, frameHeight: 48 });
   }
 
   create() {
     this.add.image(0, 0, 'background').setOrigin(0, 0);
     this.loadLevel(this.cache.json.get('level:1'));
+    const player = this.physics.add.sprite(100, 450, 'dude');
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
   }
 
   loadLevel(data) {
