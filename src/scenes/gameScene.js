cd4a918d3ba34c2267ae46ca527d6d200bba53d9
damiecode <codenlyn@gmail.com>
2020-03-12 09:16:35 +0100

    Add platforms to game scene

--- src/scenes/gameScene.js
+++ src/scenes/gameScene.js
@@ -7,9 +7,25 @@ export default class extends Phaser.Scene {
 
   preload() {
     this.load.image('background', './assets/images/background.png');
+    this.load.json('level:0', './assets/data/level00.json');
+    this.load.image('ground', './assets/images/ground.png');
+    this.load.image('grass:8x1', './assets/images/grass_8x1.png');
+    this.load.image('grass:6x1', './assets/images/grass_6x1.png');
+    this.load.image('grass:4x1', './assets/images/grass_4x1.png');
+    this.load.image('grass:2x1', './assets/images/grass_2x1.png');
+    this.load.image('grass:1x1', './assets/images/grass_1x1.png');
   }
 
   create() {
     this.add.image(0, 0, 'background').setOrigin(0, 0);
+    this.loadLevel(this.cache.json.get('level:1'));
+  }
+
+  loadLevel(data) {
+    data.platforms.forEach(this.spawnPlatform, this);
+  }
+
+  spawnPlatform(platform) {
+    this.game.add.sprite(platform.x, platform.y, platform.image);
   }
 }
