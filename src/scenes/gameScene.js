77fa11cf5f8c02af1018c05b36a6687f08150b3b
damiecode <codenlyn@gmail.com>
2020-03-12 16:42:18 +0100

    Improve platforms

--- src/scenes/gameScene.js
+++ src/scenes/gameScene.js
@@ -23,30 +23,6 @@ export default class extends Phaser.Scene {
     const player = this.physics.add.sprite(100, 450, 'dude');
     this.add.image(0, 0, 'background').setOrigin(0, 0);
     this.loadLevel(this.cache.json.get('level:1'));
-    player.anchor.set(0.5, 0.5);
-    player.setBounce(0.2);
-    player.setCollideWorldBounds(true);
-    this.anims.create({
-      key: 'left',
-      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-      frameRate: 10,
-      repeat: -1,
-    });
-
-    this.anims.create({
-      key: 'turn',
-      frames: [{ key: 'dude', frame: 4 }],
-      frameRate: 20,
-    });
-
-    this.anims.create({
-      key: 'right',
-      frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-      frameRate: 10,
-      repeat: -1,
-    });
-
-    const cursors = this.input.keyboard.createCursorKeys();
   }
 
   loadLevel(data) {
@@ -55,25 +31,5 @@ export default class extends Phaser.Scene {
 
   spawnPlatform(platform) {
     this.add.sprite(platform.x, platform.y, platform.image);
-  }
-
-  update() {
-    if (this.cursors.left.isDown) {
-      this.player.setVelocityX(-160);
-
-      this.player.anims.play('left', true);
-    } else if (this.cursors.right.isDown) {
-      this.player.setVelocityX(160);
-
-      this.player.anims.play('right', true);
-    } else {
-      this.player.setVelocityX(0);
-
-      this.player.anims.play('turn');
-    }
-
-    if (this.cursors.up.isDown && this.player.body.touching.down) {
-      this.player.setVelocityY(-330);
-    }
   }
 }
