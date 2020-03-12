5b5f626d9925a7d291f1ca81683e18890221eaf3
damiecode <codenlyn@gmail.com>
2020-03-12 21:24:26 +0100

    Add main character to the game

--- src/animations/heroanimations.js
+++ src/animations/heroanimations.js
@@ -1,20 +1,20 @@
-export default function makeAnimations() {
-  this.anims.create({
+export default function makeAnimations(scene) {
+  scene.anims.create({
     key: 'left',
-    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+    frames: scene.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
     frameRate: 10,
     repeat: -1,
   });
 
-  this.anims.create({
+  scene.anims.create({
     key: 'turn',
     frames: [{ key: 'dude', frame: 4 }],
     frameRate: 20,
   });
 
-  this.anims.create({
+  scene.anims.create({
     key: 'right',
-    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+    frames: scene.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
     frameRate: 10,
     repeat: -1,
   });
