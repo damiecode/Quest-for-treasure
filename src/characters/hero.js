a9d0dbd6dc6a59e614bc8a8a041146e40c0345a6
damiecode <codenlyn@gmail.com>
2020-03-13 17:13:56 +0100

    recreate main character

--- src/characters/hero.js
+++ src/characters/hero.js
@@ -1,8 +1,17 @@
 import Phaser from 'phaser';
 
 
-export default class Hero extends Phaser.Sprite {
+class Hero extends Phaser.Sprite {
   constructor(game, x, y) {
     super(game, x, y, 'hero');
+    this.anchor.set(0.5, 0.5);
+
+    this.game.physics.enable(this);
+    this.body.collideWorldBounds = true;
+  }
+
+  move(direction) {
+    const SPEED = 200;
+    this.body.velocity.x = direction * SPEED;
   }
 }
