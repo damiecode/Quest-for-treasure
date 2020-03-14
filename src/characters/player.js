d6510c46bdef8ddf88c64d6f8afce13358a4e4f9
damiecode <codenlyn@gmail.com>
2020-03-14 20:07:28 +0100

    Add player factory

--- src/characters/player.js
+++ src/characters/player.js
@@ -1,16 +1,17 @@
-const PlayerFactory = (player, coins, platforms, enemy, door, key) => {
+import Phaser from 'phaser';
+
+class PlayerFactory extends Phaser.Scene = (player, coins, platforms, enemy, door, key) {
+  create() {
   player = this.physics.add.sprite(100, 450, 'dude');
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
   this.physics.add.collider(player, platforms);
   this.physics.add.collider(player, door);
   this.physics.add.overlap(player, coins, this.collectCoin, null, this);
   this.physics.add.overlap(player, key, this.collectKey, null, this);
   this.physics.add.overlap(player, door, this.openDoor,
     (player, door) => this.hasKey && player.body.touching.down, this);
-
-  return (() => {
-    player.setBounce(0.2);
-    player.setCollideWorldBounds(true);
-  });
+  }
 };
 
 export default PlayerFactory;
