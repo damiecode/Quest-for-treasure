f9f5ec10405fff9b3e3176d1fe644985af607176
damiecode <codenlyn@gmail.com>
2020-03-14 20:10:43 +0100

    Add player factory

--- src/characters/player.js
+++ src/characters/player.js
@@ -1,17 +1,17 @@
 import Phaser from 'phaser';
 
-class PlayerFactory extends Phaser.Scene = (player, coins, platforms, enemy, door, key) {
-  create() {
-  player = this.physics.add.sprite(100, 450, 'dude');
-  player.setBounce(0.2);
-  player.setCollideWorldBounds(true);
-  this.physics.add.collider(player, platforms);
-  this.physics.add.collider(player, door);
-  this.physics.add.overlap(player, coins, this.collectCoin, null, this);
-  this.physics.add.overlap(player, key, this.collectKey, null, this);
-  this.physics.add.overlap(player, door, this.openDoor,
-    (player, door) => this.hasKey && player.body.touching.down, this);
+class PlayerFactory extends Phaser.Scene {
+  create(player, coins, platforms, enemy, door, key) {
+    player = this.physics.add.sprite(100, 450, 'dude');
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+    this.physics.add.collider(player, platforms);
+    this.physics.add.collider(player, door);
+    this.physics.add.overlap(player, coins, this.collectCoin, null, this);
+    this.physics.add.overlap(player, key, this.collectKey, null, this);
+    this.physics.add.overlap(player, door, this.openDoor,
+      (player, door) => this.hasKey && player.body.touching.down, this);
   }
-};
+}
 
 export default PlayerFactory;
