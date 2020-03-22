e276fceea4d84a066d4f72ec5169759bc76b35b4
damiecode <codenlyn@gmail.com>
2020-03-22 11:36:40 +0100

    Adjust configurations

--- src/index.js
+++ src/index.js
@@ -10,13 +10,14 @@ import OptionsScene from './scenes/optionsScene';
 import InstructionsScene from './scenes/instructionScene';
 import GameScene from './scenes/gameScene';
 import GameScene2 from './scenes/gameScene2';
+import PlayerName from './scenes/playerName';
 import ScoresScene from './scenes/scoreScene';
 
 import config from './config';
 
 const gameConfig = Object.assign(config, {
   scene: [BootScene, PreloaderScene, TitleScene, OptionsScene, InstructionsScene, GameScene,
-    GameScene2, ScoresScene],
+    GameScene2, PlayerName, ScoresScene],
 });
 
 
